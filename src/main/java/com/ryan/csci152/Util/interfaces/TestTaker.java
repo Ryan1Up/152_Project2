684ff0cffd10b3a5c5a9bb54582b2fa2d83a7929
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-05-11 02:19:53 -0700

    Removing v2

--- src/main/java/com/ryan/csci152/Util/interfaces/TestTaker.java
+++
@@ -1,8 +0,0 @@
-package com.ryan.csci152.Util.interfaces;
-
-import com.ryan.csci152.assessments.interfaces.Assessment;
-
-public interface TestTaker<Value>{
-
-    void doAssessment(Assessment<Value> assessment);
-}
